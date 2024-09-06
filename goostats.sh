84be3a535c06e015717e8cfde2ac5cd43dcf5b43
=Heerah <21425817@student.curtin.edu.au>
2024-09-06 11:50:37 +0800

    add welcome message and collect the filename

--- goostats.sh
+++ goostats.sh
@@ -1,2 +1,6 @@
 # Load a given file
 # computer the min/max/range of values in a file
+fname=$1
+echo "working with the file:"
+echo "welcome to Nell's stats script"
+# compute the min/max/range of value in the file 

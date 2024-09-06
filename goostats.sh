2be30bdce8c9c57c7a8dc98f6e14cba6c7127305
=Heerah <21425817@student.curtin.edu.au>
2024-09-06 15:19:43 +0800

    calculate the min value

--- goostats.sh
+++ goostats.sh
@@ -1,6 +1,8 @@
+
 # Load a given file
 # computer the min/max/range of values in a file
 fname=$1
 echo "working with the file: ${fname}"
 echo "welcome to Nell's stats script"
 # compute the min/max/range of value in the file 
+min=$(cat ${fname} |sort |head _1)
